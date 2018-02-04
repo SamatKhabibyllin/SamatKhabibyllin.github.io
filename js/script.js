f3e8f139ee81a9b4e25ba3d97450efb951624551
SamatKhabibyllin <SamatKhabibyllin@gmail.com>
2018-02-04 17:30:00 +0300

    hamburger menu active

--- js/script.js
+++ js/script.js
@@ -1,4 +1,20 @@
-//acco menu
+//main nav
+var ham = document.querySelector('.ham__btn');
+var nav = document.querySelector('.nav-list');
+var navItem = document.querySelectorAll('ul > .nav__item');
+var navLink = document.querySelectorAll('li > .nav__link');
+
+
+ham.addEventListener('click', function(e){
+   e.preventDefault();    
+   nav.classList.toggle('nav--active');
+   for(i=0;i<6;i++){
+       navItem[i].classList.toggle('nav__item--active');
+       navLink[i].classList.toggle('nav__link--active');
+   };
+});
+
+//menu acco
 var menuItem = document.querySelector('.menu-acco__item-meat');
 var menuText = document.querySelector('.acco__text-meat');
 var title = document.querySelector('.acco__title');
@@ -6,10 +22,12 @@ var link = document.querySelector('.menu-acco__link');
 
 menuItem.addEventListener('click',function(){
    console.log('клик на меню');
-   title.classList.add('acco__title--active');
-   menuText.classList.add('acco__text--active');
+   title.classList.toggle('acco__title--active');
+   menuText.classList.toggle('acco__text--active');
 });
 
 link.addEventListener('click',function(event){
    event.preventDefault(); 
 });
+
+// checkbox
